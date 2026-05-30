febf78d230b4399fa310409ca99c1ef0d98c87a5
vandit004 <vandit.s004@gmail.com>
2026-05-30 15:44:37 +0530

    Added Transaction validation

--- ATMInterface/BankAccount.java
+++ ATMInterface/BankAccount.java
@@ -7,12 +7,28 @@ public class BankAccount {
     }
 
     public void deposit(double amount) {
+
+        if (amount <= 0) {
+            System.out.println("Invalid deposit amount.");
+            return;
+        }
+
         balance += amount;
         System.out.println("Deposited: Rs. " + amount);
-
     }
 
     public void withdraw(double amount) {
+
+        if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount.");
+            return;
+        }
+
+        if (amount > balance) {
+            System.out.println("Insufficient balance.");
+            return;
+        }
+
         balance -= amount;
         System.out.println("Withdrawn: Rs. " + amount);
     }
