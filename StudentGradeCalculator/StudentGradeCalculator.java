d8e9f887903892867cda4f9edb0db88da6189258
vandit004 <vandit.s004@gmail.com>
2026-05-30 12:15:49 +0530

    Created student grade Calculator

---
+++ StudentGradeCalculator/StudentGradeCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = sc.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= subjects; i++) {
+
+            System.out.print("Enter marks for subject " + i + ": ");
+            int marks = sc.nextInt();
+
+            totalMarks += marks;
+        }
+
+        System.out.println("Total Marks: " + totalMarks);
+
+        sc.close();
+    }
+}
