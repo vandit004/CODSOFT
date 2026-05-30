080d67d25f394058c9f6fd39bc442afb165ac10c
vandit004 <vandit.s004@gmail.com>
2026-05-30 12:00:26 +0530

    Added attempt limit

--- NumberGame/NumberGame.java
+++ NumberGame/NumberGame.java
@@ -1,3 +1,4 @@
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,11 +12,12 @@ public class NumberGame {
         int number = random.nextInt(100) + 1;
         int guess = 0;
         int attempts = 0;
+        int maxAttempts = 7;
 
         System.out.println("Welcome to Number Game!");
         System.out.println("Guess a number between 1 and 100");
 
-        while (guess != number) {
+        while (guess != number && attempts < maxAttempts) {
 
             System.out.print("Enter your guess: ");
             guess = sc.nextInt();
@@ -30,6 +32,10 @@ public class NumberGame {
                 System.out.println("Congratulations!");
                 System.out.println("You guessed the number in " + attempts + " attempts.");
             }
+        }
+        if (guess != number) {
+            System.out.println("Game Over!");
+            System.out.println("The correct number was: " + number);
         }
 
         sc.close();
